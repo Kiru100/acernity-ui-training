778b3ca108c48e12342fea929a6273bf05fd98f7
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-10-10 00:03:13 +0800

    added apple cards carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
 import { AuroraBackgroundDemo } from "./components/AuroraBackground";
 import { HeroParallaxDemo } from "./components/HeroParalax";
 import { ThreeDCardDemo } from "./components/ThreeDCard";
+import { AppleCardsCarouselDemo } from "./components/AppleCardCarousel";
+
+
 
 export default function Home() {
+	useEffect(() => {
+		// Add the 'dark' class to the <html> tag by default
+		document.documentElement.classList.add('dark');
+	  }, []);
+
 	return (
 		<div className="">
 			<HeroParallaxDemo />
 			<ThreeDCardDemo/>
 			<AuroraBackgroundDemo/>
+			<AppleCardsCarouselDemo/>
 		</div>
 	);
 }
