45e4d78b9ff1cd0faf22b590f36f20bf43c0ce25
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-10-09 23:46:13 +0800

    added hero paralax

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { AuroraBackgroundDemo } from "./components/AuroraBackground";
+import { HeroParallaxDemo } from "./components/HeroParalax";
 import { ThreeDCardDemo } from "./components/ThreeDCard";
 
 export default function Home() {
 	return (
 		<div className="">
+			<HeroParallaxDemo />
 			<ThreeDCardDemo/>
+			<AuroraBackgroundDemo/>
 		</div>
 	);
 }
